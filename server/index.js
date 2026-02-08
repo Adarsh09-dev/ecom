117ad6fd699c9117b6d67692ff9f94e7b19144cc
Adarsh sasi <adarshsasi099@gmail.com>
2026-02-08 14:31:59 +0530

    add express-ejs-layouts for EJS templating support

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import expressEjsLayouts from 'express-ejs-layouts';
 import cors from 'cors';
 import dotenv  from 'dotenv';
 dotenv.config();
@@ -6,7 +7,7 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import connectDB from './config/connectDB.js';
-import userRouter from './route/user.route.js';
+import userRouter from './route/user.route.js'
 // await connectDB();
 
 
@@ -23,6 +24,12 @@ app.use(helmet({
     crossOriginResourcePolicy: false
 }));
 
+//  EJS Templating Engine Setup
+app.set("views", path.join(__dirname, "views"));
+app.use(expressEjsLayouts);
+app.set("layout", "layouts/main");
+app.set("view engine", "ejs");
+
 
  
 
@@ -30,6 +37,7 @@ app.get('/', (req, res) => {
   res.json({ message: "server running" });
 });
 
+// user router
 app.use('/api/user',userRouter)
 
  connectDB().then(() => {
