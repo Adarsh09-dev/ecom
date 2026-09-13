bcc5741ec7f1eb2c888098ae93ec5e4f3e6380a1
Adarsh09-dev <adarshsasi.dev@gmail.com>
2026-09-13 09:43:14 +0530

    feat: Refactor address routes and remove inline toast styles from main layout

--- server/route/address.route.js
+++ server/route/address.route.js
@@ -1,5 +1,32 @@
 import { Router } from 'express'
+import {
+    addressListPage,
+    addAddressPage,
+    addAddressController,
+    checkoutPage,
+    updateAddressController,
+    deleteAddressController,
+    updateAddressPage,
+
+
+} from '../controllers/addressController.js'
+
 
 const addressRouter = Router()
+
+addressRouter.get("/add-address", addAddressPage);
+addressRouter.post("/add-address", addAddressController);
+addressRouter.get("/checkout", checkoutPage);
+addressRouter.get("/",addressListPage);
+addressRouter.get("/edit/:id",updateAddressPage)
+addressRouter.put("/update/:id",updateAddressController);
+addressRouter.delete("/:id",deleteAddressController)
+
+
+
+
+
+
+
 
 export default addressRouter
