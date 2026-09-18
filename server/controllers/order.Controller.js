0e528f055e690d780b942f69b3e708c348e9788f
Adarsh09-dev <adarshsasi.dev@gmail.com>
2026-09-18 16:59:22 +0530

    feat: Enhance CashOnDeliveryOrderController to include detailed order payload and address handling

--- server/controllers/order.Controller.js
+++ server/controllers/order.Controller.js
@@ -1,39 +1,90 @@
 import OrderModel from "../models/Order-Model.js";
 import UserModel from "../models/User-Model.js";
+import CartProductModel from "../models/cartProduct-Model.js"
+import AddressModel from "../models/Address-Model.js";
+import mongoose from "mongoose";
+
 
 
 export const CashOnDeliveryOrderController = async (req, res) => {
-
     try {
 
-        const userId = req.userId // auth
-        const { list_items, totalAmt, addressId, subTotalAmt } = req.body
-
-
-        // userId:
-        // orderId:
-        // productId:
-        // product_detials: {
-        //     name : "",
-        //     image : ""
-        // }
-        // paymentId:
-        // paymenat_status: ery_address:
-        // subTotalAmt:
-        // totalAmt:
-        // Invoice_recept:
+        const userId = req.session.user.id;
+        const {
+            list_items,
+            totalAmt,
+            addressId,
+            subTotalAmt
+        } = req.body;
 
 
 
+        console.log("========== COD ORDER ==========");
 
+        console.log("USER ID =", userId);
+        console.log("REQ.BODY =", req.body);
+        console.log("REQ.PARAMS =", req.params);
+
+
+        console.log("list_items =", list_items);
+        console.log("totalAmt =", totalAmt);
+        console.log("addressId =", addressId);
+        console.log("subTotalAmt =", subTotalAmt);
+
+        const payload = list_items.map(el => {
+            return ({
+
+                userId: userId,
+
+                orderId: `ORD-${new mongoose.Types.ObjectId()}`,
+
+                productId: el.productId._id,
+
+                product_detials: {
+                    name: el.productId.name,
+                    Image: el.productId.Image,
+                },
+
+                paymentId: "",
+
+                paymenat_status: "CASH ON DELIVERY",
+
+                delivery_address: addressId,
+
+                subTotalAmt: subTotalAmt,
+                totalAmt: totalAmt,
+
+                // Invoice_recept :
+
+
+            })
+        })
+
+         const generatedOrder = await OrderModel.insertMany(payload);
+
+         /// remove from the cart
+         const removeCartItems = CartProductModel.deleteMany({ userId : userId})
+         const updateInUser = UserModel.updateOne( {_id : userId}, {shopping_cart : []})
+
+
+
+         return res.json({
+            message : "Order successfully",
+            error : false,
+            success : true,
+            data : generatedOrder
+         })
+
+        console.log("===============================");
 
     } catch (error) {
+
+        console.log("COD ERROR =", error);
+
         return res.status(500).json({
-
             message: error.message || error,
-            errro: true,
+            error: true,
             success: false
-
-        })
+        });
     }
-}
+};
